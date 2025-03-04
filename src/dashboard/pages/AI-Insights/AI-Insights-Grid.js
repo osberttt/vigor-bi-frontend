01271a45880b8e64e543d57228888935e2b11691
Min Htet Naing <minhtetnaing25mhn@gmail.com>
2025-03-04 10:06:10 +0700

    added ai logic

--- src/dashboard/pages/AI-Insights/AI-Insights-Grid.js
+++ src/dashboard/pages/AI-Insights/AI-Insights-Grid.js
@@ -4,6 +4,20 @@ import Copyright from '../../internals/components/Copyright';
 import { Card, CardContent, Typography } from '@mui/material';
 
 export default function AIGrid() {
+
+  const [insight, setInsight] = useState("AI Insight hasn't been implemented yet.");
+    useEffect(() => {
+      // Fetch the API data
+      fetch(`http://localhost:5000/api/ai-insight/insight`)
+        .then((response) => response.json())
+        .then((data) => {
+          setInsight(data.data.insight);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }, []);
+
   return (
     <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' } }}>
       <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
@@ -12,7 +26,7 @@ export default function AIGrid() {
       <Card variant="outlined" sx={{ maxWidth: 300 }}>
       <CardContent>
         <Typography variant="body1">
-          AI Insight hasn't been implemented yet.
+          {insight}
         </Typography>
       </CardContent>
     </Card>
